96baf0f6fbc3d77a1c4996f25f318313891b4a4f
Ritesh Verma <riteshv5678@gmail.com>
2025-12-28 21:58:32 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, Ritesh!");
+        System.out.println("Hello, Ritesh!, Rahul, Sudha ");
     }
 }
